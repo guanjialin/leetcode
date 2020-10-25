6f59b6d0f4b563d7a656e0bd04e7911cf0719ea3
guanjialin <guanjialin@dbteam.top>
2020-10-25 23:29:35 +0800

    add solution 1171

---
+++ 1171.remove-zero-sum-consecutive-nodes-from-linked-list.cpp
@@ -0,0 +1,46 @@
+//
+// Created by dell on 2020/10/21.
+//
+
+#include "cppheader.h"
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeZeroSumSublists(ListNode* head) {
+        auto *dummy = new ListNode(0);
+        dummy->next = head;
+        std::unordered_map<int, ListNode *> unmap;
+
+        int sum = 0;
+        for (auto d = dummy; d != nullptr; d = d->next) {
+            sum += d->val;
+            unmap[sum] = d;
+        }
+
+        sum = 0;
+        for (auto d = dummy; d != nullptr; d = d->next) {
+            sum += d->val;
+            d->next = unmap[sum]->next;
+        }
+
+        return dummy->next;
+    }
+};
+
+int main()
+{
+    vector<int> data = {1, 2, 3, -3, -2};
+    auto ans = Solution().removeZeroSumSublists(ListNode::create(data));
+    ans->print();
+
+    return 0;
+}
