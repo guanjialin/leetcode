ed0c6354efc477f9424346216b46f05a0a2a35a9
guanjialin <guanjialin@dbteam.top>
2020-11-06 12:10:16 +0800

    add solution 1598

---
+++ 1598.crawler-log-folder.cpp
@@ -0,0 +1,30 @@
+//
+// Created by addzero on 2020/11/6.
+//
+
+
+#include "cppheader.h"
+
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int ans = 0;
+
+        for (auto log : logs) {
+            if (log == "../") {
+                ans += ans > 0 ? -1 : 0;
+            } else if (log != "./") {
+                ++ans;
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    vector<string> logs = {"d1/","d2/","./","d3/","../","d31/"};
+    cout << Solution().minOperations(logs) << endl;
+
+    return 0;
+}
