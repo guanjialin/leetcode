6401cd3a4c4bc93c20f7ff0364e930907ce02cf3
guanjialin <guanjialin@dbteam.top>
2020-11-06 12:06:09 +0800

    add solution 1332

---
+++ 1332.remove-palindromic-subsequences.cpp
@@ -0,0 +1,36 @@
+//
+// Created by addzero on 2020/11/6.
+//
+
+
+#include "cppheader.h"
+
+// TODO 用二分搜索应该会更快
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        if (s.empty()) {
+            return 0;
+        }
+
+        // 如果只包含一种字母，则必定为回文
+        if (isPalindrome(s)) {
+            return 1;
+        }
+
+        return 2;
+    }
+
+private:
+    bool isPalindrome(const string &s) {
+        string r = s;
+        std::reverse(r.begin(), r.end());
+        return s == r;
+    }
+};
+
+int main() {
+    cout << Solution().removePalindromeSub("ababa") << endl;
+
+    return 0;
+}
