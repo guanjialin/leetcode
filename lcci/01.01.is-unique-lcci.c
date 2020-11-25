c1a005add7a87b4dc60232a94b79420e4b85bdd9
guanjialin <guanjialin@dbteam.top>
2020-11-25 23:06:33 +0800

    add solution lcci 01.01

---
+++ lcci/01.01.is-unique-lcci.c
@@ -0,0 +1,48 @@
+//
+// Created by addzero on 2020/11/25.
+//
+
+
+#include "../header.h"
+
+#ifdef __QSORT__
+// 使用排序的解法
+static int cmp(const void *x, const void *y)
+{
+    return *(char *) x - *(char *) y;
+}
+
+bool isUnique(char *astr)
+{
+    int len = strlen(astr);
+
+    qsort(astr, len, sizeof(char), cmp);
+
+    for (int i = 0; i < len - 1; ++i) {
+        if (astr[i] == astr[i + 1]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+#else
+// 使用数组计数的解法
+bool isUnique(char* astr){
+    int count[128] = {0};
+
+    while (*astr != '\0') {
+        if (count[*astr]++ >= 1) {
+            return false;
+        }
+        ++astr;
+    }
+    return true;
+}
+#endif
+
+int main() {
+
+
+    return 0;
+}
